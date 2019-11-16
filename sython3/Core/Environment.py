34d288fd16747a13452cc26f22816229decd3389
LavaPower <lavapower84@gmail.com>
2019-11-16 17:53:21 +0100

    Add round function

--- sython3/Core/Environment.py
+++ sython3/Core/Environment.py
@@ -5,10 +5,10 @@ class Environment:
     def __init__(self):
         self.operators = {
             '+': op.add, '-': op.sub, "/": op.truediv, "*": op.mul, "//": op.floordiv, "%": op.mod,
-            '**': pow, '=': self.set, "<=": op.le, "<": op.lt, ">": op.gt, ">=": op.ge, "==": op.eq, "!=": op.ne
+            '**': op.pow, '=': self.set, "<=": op.le, "<": op.lt, ">": op.gt, ">=": op.ge, "==": op.eq, "!=": op.ne
         }
         self.functions = {
-            'print': print, "int": int, 'float': float, 'str': str
+            'print': print, "int": int, 'float': float, 'str': str, 'round': round
         }
         self.variables = {
             "true": True,
