485cab4e6f1fa859da8f2e350f097491d14897c3
LavaPower <lavapower84@gmail.com>
2019-11-16 12:42:08 +0100

    Add ";" in Parser and rework on it

--- Core/Parser.py
+++ Core/Parser.py
@@ -5,16 +5,26 @@ from Core.Utils import split
 
 class Parser:
     def __init__(self, program):
-        self.program = program
-        blocked_left_regex_symbols = ("!<>", "", "", "", "", "", "", "", "", "", "", "", "", "")
-        blocked_right_regex_symbols = ("", "", "", "", "", "", "", "", "", "", "", "", "=", "=")
-        regex_symbols = ("=", ")", '"', "'", "\n", "+", "-", "/", "*", "%", "(", ",", "<", ">")
-        other_symbols = ("**", "//", "<=", ">=", "==", "!=")
-        for k, v in enumerate(regex_symbols):
-            regex = re.compile(r"([^{0}])([{1}])([^{2}]|$)".format(v+blocked_left_regex_symbols[k], v,
-                                                                   v+blocked_right_regex_symbols[k]))
-            self.program = regex.sub("\\1 \\2 \\3", self.program)
-        for i in other_symbols:
+        self.program = program.replace("\n", "")
+        self.symbols = ("=", ")", '"', "'", ";", "+", "-", "/", "*", "%", "(", ",", "<", ">", "**", "//", "<=", ">=",
+                        "==", "!=")
+        self.regex_symbols = (("!<>=", "=", "="), ("", ")", ""), ("", '"', ""), ("", "'", ""), ("", ";", ""),
+                              ("", "+", ""), ("", "-", ""), ("/", "/", "/"), ("*", "*", "*"), ("", "%", ""),
+                              ("", "(", ""), ("", ",", ""), ("", "<", "="), ("", ">", "="))
+        for i in self.regex_symbols:
+            if i[0] != "" and i[2] != "":
+                regex = re.compile(r"([^{0}]|^)([{1}])([^{2}]|$)".format(*i))
+                self.program = regex.sub("\\1 \\2 \\3", self.program)
+            elif i[2] != "":
+                regex = re.compile(r"([{0}])([^{1}]|$)".format(*i[1:]))
+                self.program = regex.sub(" \\1 \\2", self.program)
+            elif i[0] != "":
+                regex = re.compile(r"([^{0}]|^)([{1}])".format(*i[:-1]))
+                self.program = regex.sub("\\1 \\2 ", self.program)
+            else:
+                regex = re.compile(r"([{0}])".format(i[1]))
+                self.program = regex.sub(" \\1 ", self.program)
+        for i in (i for i in self.symbols if len(i) == 2):
             self.program = self.program.replace(i, " " + i + " ")
 
     def tokenize(self):
@@ -49,28 +59,36 @@ class Parser:
         return tlist
 
     def parse(self):
-        return self.read_from_tokens(self.tokenize(), 0)
+        return self.read_from_tokens(self.tokenize(), False)
 
-    def read_from_tokens(self, tokens, nb):
+    def read_from_tokens(self, tokens, return_semi_colon):
         if len(tokens) == 0:
             return []
         liste = []
         while len(tokens):
             token = tokens.pop(0)
             if token == "(":
-                liste.append(self.read_from_tokens(tokens, nb + 1))
+                liste.append(self.read_from_tokens(tokens, False))
             elif token == ")":
                 return liste
             elif token == "=":
                 liste.append(self.get_value(token))
-                liste.append(self.read_from_tokens(tokens, nb + 1))
-                liste.append(self.get_value("\n"))
-            elif token == "\n" and nb != 0:
+                liste.append(self.read_from_tokens(tokens, True))
+                liste.append(self.get_value(";"))
+            elif token == ";" and return_semi_colon:
                 return liste
             else:
                 liste.append(self.get_value(token))
 
-        liste = split(liste, "\n")
+        liste = split(liste, ";")
+        for i in self.symbols:
+            while isinstance(liste, list) and len(liste) == 1:
+                liste = liste[0]
+            temp = split(liste, i, False)
+            for k, v in enumerate(temp):
+                if len(v) == 1:
+                    temp[k] = v[0]
+            liste = temp
 
         return liste
 
