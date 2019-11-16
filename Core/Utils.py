0f4c042989ee6aec18ad8e028dabb8582a40b177
LavaPower <lavapower84@gmail.com>
2019-11-16 12:40:43 +0100

    Remake split function to save mot or not

--- Core/Utils.py
+++ Core/Utils.py
@@ -1,12 +1,22 @@
 import itertools
 
 
-def split(liste, mot):
-    return list(filter(
-        lambda n: not isinstance(n[0], str) or (n[0].strip() != "" and n[0] != mot),
-        map
-            (
-            lambda n: list(n[1]),
-            itertools.groupby(liste, lambda word: isinstance(word, str) and word == mot)
-        )
-    ))
+def split(liste, mot, exclu=True):
+    if exclu:
+        return list(filter(
+            lambda n: not isinstance(n[0], str) or (n[0].strip() != "" and n[0] != mot),
+            map
+                (
+                lambda n: list(n[1]),
+                itertools.groupby(liste, lambda word: isinstance(word, str) and word == mot)
+            )
+        ))
+    else:
+        return list(filter(
+            lambda n: not isinstance(n[0], str) or (n[0].strip() != ""),
+            map
+                (
+                lambda n: list(n[1]),
+                itertools.groupby(liste, lambda word: isinstance(word, str) and word == mot)
+            )
+        ))
