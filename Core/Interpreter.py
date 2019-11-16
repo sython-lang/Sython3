66938503951d5c277e62a6145c3c2ea67ceae7e5
LavaPower <lavapower84@gmail.com>
2019-11-16 12:42:21 +0100

    Better management of arguments

--- Core/Interpreter.py
+++ Core/Interpreter.py
@@ -24,6 +24,8 @@ class Interpreter:
             self.eval(parser.parse())
 
     def eval(self, prog):
+        while isinstance(prog, list) and len(prog) == 1:
+            prog = prog[0]
         for k, v in enumerate(prog):
             self.eval_exp(k, v)
 
@@ -41,14 +43,32 @@ class Interpreter:
         elif len(exp) == 1:
             return self.eval_exp(nb, exp[0])
         else:
-            if exp[0] in self.env.functions.keys():
+            if isinstance(exp[0], str) and exp[0] in self.env.functions.keys():
                 proc = self.env.functions[exp[0]]
-                args = [self.eval_exp(nb, i) for i in split(exp[1], ",")]
+                temp = split(exp[1], ",")
+                for k, v in enumerate(temp):
+                    if len(v) == 1:
+                        temp[k] = v[0]
+                args = temp
+                for k, v in enumerate(args):
+                    if isinstance(v, list):
+                        for i in self.env.operators.keys():
+                            if i in v:
+                                temp = split(v, i, False)
+                                for kt, vt in enumerate(temp):
+                                    if len(vt) == 1:
+                                        temp[kt] = vt[0]
+                                args[k] = self.eval_exp(nb, temp)
+                    else:
+                        args[k] = self.eval_exp(nb, v)
                 return proc(*args)
-            elif exp[1] in self.env.operators.keys():
+            elif isinstance(exp[1], str) and exp[1] in self.env.operators.keys():
                 proc = self.env.operators[exp[1]]
                 if exp[1] == "=":
                     args = (exp[0], self.eval_exp(nb, exp[2]))
                 else:
                     args = (self.eval_exp(nb, exp[0]), self.eval_exp(nb, exp[2]))
-                return proc(*args)
+                try:
+                    return proc(*args)
+                except Exception as e:
+                    print(ERROR_STR.format(e.__class__.__name__, nb, " ".join(e.args).capitalize()))
